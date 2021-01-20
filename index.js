3befb8ddb512ad22afad8331e38a1f7d151f6b05
Hyper3557 <arjuoon@gmail.com>
2021-01-20 13:48:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-prefix = '--'
-client.once('ready', () => {
-    console.log('I am ready');
-});  
+const fs = require('fs');
+const discord = require('discord.js');
 
-client.login('ODAxMzM0ODI1NDk5ODg1NTcw.YAfLNg.MBV4vOAlViMuo_3if9t1MeIzyLA');
+const client = new discord.Client({ disableMentions: 'everyone' });
 
-client.on('message', message => {
-    if(message.content === prefix + 'Testing'){
-        message.channel.send('Taosting');
-    }else if(message.content === prefix + 'server name'){
-        message.channel.send(message.guild.name);
-    }else if(message.content === prefix + 'server member'){
-        message.channel.send("Total Members: " + message.guild.memberCount);
-    }
-})
+const { Player } = require('discord-player');
+
+client.player = new Player(client);
+client.config = require('./config/bot');
+client.emotes = client.config.emojis;
+client.filters = client.config.filters;
+client.commands = new discord.Collection();
+
+fs.readdirSync('./commands').forEach(dirs => {
+    const commands = fs.readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
+
+    for (const file of commands) {
+        const command = require(`./commands/${dirs}/${file}`);
+        console.log(`Loading command ${file}`);
+        client.commands.set(command.name.toLowerCase(), command);
+    };
+});
+
+const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const player = fs.readdirSync('./player').filter(file => file.endsWith('.js'));
+
+for (const file of events) {
+    console.log(`Loading discord.js event ${file}`);
+    const event = require(`./events/${file}`);
+    client.on(file.split(".")[0], event.bind(null, client));
+};
+
+for (const file of player) {
+    console.log(`Loading discord-player event ${file}`);
+    const event = require(`./player/${file}`);
+    client.player.on(file.split(".")[0], event.bind(null, client));
+};
+
+client.login(client.config.discord.token);
